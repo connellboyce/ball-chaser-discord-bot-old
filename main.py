3f099584defcd8e113b6a14af667aecbc3a2b5e8
Connell Boyce <>
2021-04-22 18:11:40 +0000

    created dynamic rank retrieval methods

--- main.py
+++ main.py
@@ -18,20 +18,58 @@ async def on_message(message):
     await message.channel.send("What a save!")
 
   if message.content.startswith('!rank'):
-    rank = getRank()
+    contentList = str(message.content).split(" ")
+    if (contentList[2] == "1"):
+      rank = get1Rank(contentList[1])
+    if (contentList[2] == "2"):
+      rank = get2Rank(contentList[1])
+    if (contentList[2] == "3"):
+      rank = get3Rank(contentList[1])
+    else:
+      rank = "invalid arguments"
+
     await message.channel.send(rank)
 
-def getRank():
+def get3Rank(username):
+  print("uname="+username)
   key = os.environ['TRN_ACCESS_KEY_ID'] # Your key goes here, this is mine
   platform = 'epic' 
-  platformUserIdentifier = 'i_am_connell' # Random user example from gg tracker website
-  link = f"https://public-api.tracker.gg/v2/rocket-league/standard/profile/{platform}/{platformUserIdentifier}"
+  username = username;
+  link = f"https://public-api.tracker.gg/v2/rocket-league/standard/profile/{platform}/{username}"
   headers = {
     'TRN-Api-Key' : key,
   }
   response = requests.get(link, headers=headers)
   jsonResponse = json.loads(response.text)
   rank = jsonResponse["data"]["segments"][3]["stats"]["tier"]["metadata"]["name"]
+  return rank;
+
+def get2Rank(username):
+  print("uname="+username)
+  key = os.environ['TRN_ACCESS_KEY_ID'] # Your key goes here, this is mine
+  platform = 'epic' 
+  username = username;
+  link = f"https://public-api.tracker.gg/v2/rocket-league/standard/profile/{platform}/{username}"
+  headers = {
+    'TRN-Api-Key' : key,
+  }
+  response = requests.get(link, headers=headers)
+  jsonResponse = json.loads(response.text)
+  rank = jsonResponse["data"]["segments"][2]["stats"]["tier"]["metadata"]["name"]
+  return rank;
+
+def get1Rank(username):
+  print("uname="+username)
+  key = os.environ['TRN_ACCESS_KEY_ID'] # Your key goes here, this is mine
+  platform = 'epic' 
+  username = username;
+  link = f"https://public-api.tracker.gg/v2/rocket-league/standard/profile/{platform}/{username}"
+  headers = {
+    'TRN-Api-Key' : key,
+  }
+  response = requests.get(link, headers=headers)
+  jsonResponse = json.loads(response.text)
+  rank = jsonResponse["data"]["segments"][1]["stats"]["tier"]["metadata"]["name"]
   return rank;
 
 token = os.environ['BALL_CHASER_TOKEN']

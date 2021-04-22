c409a42514561ffdb5caade795357d0995445a23
Connell Boyce <>
2021-04-22 18:50:31 +0000

    Handling for out of order "unranked" ranking

--- main.py
+++ main.py
@@ -14,24 +14,20 @@ async def on_message(message):
   if message.author == client.user:
     return
 
+  if message.content.startswith('!help'):
+    await message.channel.send("!rank <username> <1, 2, or 3> : check your rank\n!goal : get 'get what a save-d'")
+
   if message.content.startswith('!goal'):
     await message.channel.send("What a save!")
 
   if message.content.startswith('!rank'):
     contentList = str(message.content).split(" ")
-    if (contentList[2] == "1"):
-      rank = get1Rank(contentList[1])
-    if (contentList[2] == "2"):
-      rank = get2Rank(contentList[1])
-    if (contentList[2] == "3"):
-      rank = get3Rank(contentList[1])
-    else:
-      rank = "invalid arguments"
+    rank = getRank(contentList[1], contentList[2])
 
     await message.channel.send(rank)
 
-def get3Rank(username):
-  print("uname="+username)
+def getRank(username, mode):
+  mode = int(mode)
   key = os.environ['TRN_ACCESS_KEY_ID'] # Your key goes here, this is mine
   platform = 'epic' 
   username = username;
@@ -41,35 +37,9 @@ def get3Rank(username):
   }
   response = requests.get(link, headers=headers)
   jsonResponse = json.loads(response.text)
-  rank = jsonResponse["data"]["segments"][3]["stats"]["tier"]["metadata"]["name"]
-  return rank;
-
-def get2Rank(username):
-  print("uname="+username)
-  key = os.environ['TRN_ACCESS_KEY_ID'] # Your key goes here, this is mine
-  platform = 'epic' 
-  username = username;
-  link = f"https://public-api.tracker.gg/v2/rocket-league/standard/profile/{platform}/{username}"
-  headers = {
-    'TRN-Api-Key' : key,
-  }
-  response = requests.get(link, headers=headers)
-  jsonResponse = json.loads(response.text)
-  rank = jsonResponse["data"]["segments"][2]["stats"]["tier"]["metadata"]["name"]
-  return rank;
-
-def get1Rank(username):
-  print("uname="+username)
-  key = os.environ['TRN_ACCESS_KEY_ID'] # Your key goes here, this is mine
-  platform = 'epic' 
-  username = username;
-  link = f"https://public-api.tracker.gg/v2/rocket-league/standard/profile/{platform}/{username}"
-  headers = {
-    'TRN-Api-Key' : key,
-  }
-  response = requests.get(link, headers=headers)
-  jsonResponse = json.loads(response.text)
-  rank = jsonResponse["data"]["segments"][1]["stats"]["tier"]["metadata"]["name"]
+  if (jsonResponse["data"]["segments"][1]["metadata"]["name"] == "Un-Ranked"):
+    mode = mode+1
+  rank = jsonResponse["data"]["segments"][mode]["stats"]["tier"]["metadata"]["name"]
   return rank;
 
 token = os.environ['BALL_CHASER_TOKEN']
